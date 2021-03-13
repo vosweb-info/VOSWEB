4c502481518433fbe66df44cd25c03bf6e03c5e0
Manuel Schierenberg <mail@elanum.de>
2021-03-13 14:52:10 +0100

    refactor: :truck: rename import paths
    change absolute/relative paths to "@" paths

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Navbar from './components/Navbar.vue';
+import Navbar from '@/components/Navbar.vue';
 
 @Component({
   components: {
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { BootstrapVue } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
 
-import './app.scss';
+import '@/app.scss';
 
 library.add(faUsers);
 
