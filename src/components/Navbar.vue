e5c377f6d6de7e9e5a4696dd4566619394c5ab43
Manuel Schierenberg <mail@elanum.de>
2021-03-14 00:21:26 +0100

    feat: :sparkles: add scrollspy to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,40 @@
 <template>
-  <div>
-    <b-navbar toggleable="xl" type="light" variant="light">
-      <b-container>
-        <b-navbar-brand to="/">
-          <img src="@/assets/images/vosweb-logo.png" alt="logo" height="30" />
-        </b-navbar-brand>
+  <b-navbar
+    toggleable="xl"
+    type="light"
+    variant="light"
+    class="shadow-sm"
+    sticky
+  >
+    <b-container>
+      <b-navbar-brand to="/">
+        <img src="@/assets/images/vosweb-logo.png" alt="logo" height="30" />
+      </b-navbar-brand>
 
+      <div v-if="['Home'].includes($route.name)">
         <b-navbar-toggle target="nav-collapse" />
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#about">Über uns</b-nav-item>
+          <b-navbar-nav v-b-scrollspy:home="70">
+            <b-nav-item href="#about"> Über uns </b-nav-item>
             <b-nav-item href="#solutions">IT-Lösungen</b-nav-item>
-            <b-nav-item href="#maintenance">Administration/Wartung</b-nav-item>
-            <b-nav-item href="#security">IT-Sicherheit</b-nav-item>
-            <b-nav-item href="#office365">Office365-Integration</b-nav-item>
-            <b-nav-item href="#hosting">Hosting/Webdesign</b-nav-item>
-            <b-nav-item href="#contact">Kontakt</b-nav-item>
+            <b-nav-item href="#maintenance">
+              Administration/Wartung
+            </b-nav-item>
+            <b-nav-item href="#security"> IT-Sicherheit </b-nav-item>
+            <b-nav-item href="#office365"> Office365-Integration </b-nav-item>
+            <b-nav-item href="#hosting"> Hosting/Webdesign </b-nav-item>
+            <b-nav-item href="#contact"> Kontakt </b-nav-item>
           </b-navbar-nav>
         </b-collapse>
-      </b-container>
-    </b-navbar>
-  </div>
+      </div>
+    </b-container>
+  </b-navbar>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navbar extends Vue {}
+</script>
