a15c3303bd10518efb21e642a7cf44e691ef98ca
Manuel Schierenberg <mail@elanum.de>
2021-03-13 17:21:01 +0100

    feat: :sparkles: add legal notice view

---
+++ src/views/LegalNotice.vue
@@ -0,0 +1,300 @@
+<template>
+  <div class="bg-light">
+    <b-container class="bg-white">
+      <h1>Impressum</h1>
+      <p>
+        Angaben gemäß § 5 TMG:<br />
+        Volker Schierenberg<br />
+        VOSWEB - Internetdienstleistung<br />
+        Gaisthalerhammer 6, 92539 Schönsee
+      </p>
+      <h4>Kontakt:</h4>
+      <p>
+        Telefon: 0 96 74 / 68 30 171<br />
+        E-Mail: mail@vosweb.de<br />
+      </p>
+      <h4>Umsatzsteuer-ID:</h4>
+      <p>
+        Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; §27 a
+        Umsatzsteuergesetz:<br />
+        DE277525769
+      </p>
+      <p>
+        Quelle:
+        <em>
+          Impressumgenerator von
+          <a href="http://www.e-recht24.de/"> http://www.e-recht24.de/ </a>
+        </em>
+      </p>
+      <h4>Haftungsausschluss:</h4>
+      <p>
+        <strong>Haftung f&uuml;r Inhalte</strong>
+      </p>
+      <p>
+        Die Inhalte unserer Seiten wurden mit gr&ouml;&szlig;ter Sorgfalt
+        erstellt. F&uuml;r die Richtigkeit, Vollst&auml;ndigkeit und
+        Aktualit&auml;t der Inhalte k&ouml;nnen wir jedoch keine Gew&auml;hr
+        &uuml;bernehmen. Als Diensteanbieter sind wir gem&auml;&szlig; § 7 Abs.1
+        TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen
+        Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
+        Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder
+        gespeicherte fremde Informationen zu &uuml;berwachen oder nach
+        Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit
+        hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+        Informationen nach den allgemeinen Gesetzen bleiben hiervon
+        unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem
+        Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich.
+        Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+        Inhalte umgehend entfernen.
+      </p>
+      <p>
+        <strong>Haftung f&uuml;r Links</strong>
+      </p>
+      <p>
+        Unser Angebot enth&auml;lt Links zu externen Webseiten Dritter, auf
+        deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir
+        f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen.
+        F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige
+        Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
+        wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche
+        Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte
+        waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente
+        inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
+        Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden
+        von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+      </p>
+      <p>
+        <strong>Urheberrecht</strong>
+      </p>
+      <p>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+        Seiten unterliegen dem deutschen Urheberrecht. Die
+        Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der
+        Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen
+        der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+        Downloads und Kopien dieser Seite sind nur f&uuml;r den privaten, nicht
+        kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite
+        nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
+        beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
+        Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden,
+        bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von
+        Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+      </p>
+      <p>
+        <strong>Datenschutz</strong>
+      </p>
+      <p>
+        Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+        personenbezogener Daten m&ouml;glich. Soweit auf unseren Seiten
+        personenbezogene Daten (beispielsweise Name, Anschrift oder
+        eMail-Adressen) erhoben werden, erfolgt dies, soweit m&ouml;glich, stets
+        auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdr&uuml;ckliche
+        Zustimmung nicht an Dritte weitergegeben.
+      </p>
+      <p>
+        Wir weisen darauf hin, dass die Daten&uuml;bertragung im Internet (z.B.
+        bei der Kommunikation per E-Mail) Sicherheitsl&uuml;cken aufweisen kann.
+        Ein l&uuml;ckenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+        nicht m&ouml;glich.
+      </p>
+      <p>
+        Der Nutzung von im Rahmen der Impressumspflicht ver&ouml;ffentlichten
+        Kontaktdaten durch Dritte zur &Uuml;bersendung von nicht
+        ausdr&uuml;cklich angeforderter Werbung und Informationsmaterialien wird
+        hiermit ausdr&uuml;cklich widersprochen. Die Betreiber der Seiten
+        behalten sich ausdr&uuml;cklich rechtliche Schritte im Falle der
+        unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails,
+        vor.
+      </p>
+      <p>
+        <strong
+          >Datenschutzerkl&auml;rung f&uuml;r die Nutzung von Facebook-Plugins
+          (Like-Button)</strong
+        >
+      </p>
+      <p>
+        Auf unseren Seiten sind Plugins des sozialen Netzwerks Facebook, 1601
+        South California Avenue, Palo Alto, CA 94304, USA integriert. Die
+        Facebook-Plugins erkennen Sie an dem Facebook-Logo oder dem
+        "Like-Button" ("Gef&auml;llt mir") auf unserer Seite. Eine
+        &Uuml;bersicht &uuml;ber die Facebook-Plugins finden Sie hier:
+        <a href="http://developers.facebook.com/docs/plugins/" target="_blank">
+          http://developers.facebook.com/docs/plugins/
+        </a>
+        <br />
+
+        Wenn Sie unsere Seiten besuchen, wird &uuml;ber das Plugin eine direkte
+        Verbindung zwischen Ihrem Browser und dem Facebook-Server hergestellt.
+        Facebook erh&auml;lt dadurch die Information, dass Sie mit Ihrer
+        IP-Adresse unsere Seite besucht haben. Wenn Sie den Facebook
+        "Like-Button" anklicken w&auml;hrend Sie in Ihrem Facebook-Account
+        eingeloggt sind, k&ouml;nnen Sie die Inhalte unserer Seiten auf Ihrem
+        Facebook-Profil verlinken. Dadurch kann Facebook den Besuch unserer
+        Seiten Ihrem Benutzerkonto zuordnen. Wir weisen darauf hin, dass wir als
+        Anbieter der Seiten keine Kenntnis vom Inhalt der &uuml;bermittelten
+        Daten sowie deren Nutzung durch Facebook erhalten. Weitere Informationen
+        hierzu finden Sie in der Datenschutzerkl&auml;rung von facebook unter
+        <a href="http://de-de.facebook.com/policy.php" target="_blank">
+          http://de-de.facebook.com/policy.php
+        </a>
+      </p>
+      <p>
+        Wenn Sie nicht w&uuml;nschen, dass Facebook den Besuch unserer Seiten
+        Ihrem Facebook-Nutzerkonto zuordnen kann, loggen Sie sich bitte aus
+        Ihrem Facebook-Benutzerkonto aus.
+      </p>
+      <p>
+        <strong>
+          Datenschutzerkl&auml;rung f&uuml;r die Nutzung von Google Analytics
+        </strong>
+      </p>
+      <p>
+        Diese Website benutzt Google Analytics, einen Webanalysedienst der
+        Google Inc. ("Google"). Google Analytics verwendet sog. "Cookies",
+        Textdateien, die auf Ihrem Computer gespeichert werden und die eine
+        Analyse der Benutzung der Website durch Sie erm&ouml;glichen. Die durch
+        den Cookie erzeugten Informationen &uuml;ber Ihre Benutzung dieser
+        Website werden in der Regel an einen Server von Google in den USA
+        &uuml;bertragen und dort gespeichert. Im Falle der Aktivierung der
+        IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google
+        jedoch innerhalb von Mitgliedstaaten der Europ&auml;ischen Union oder in
+        anderen Vertragsstaaten des Abkommens &uuml;ber den Europ&auml;ischen
+        Wirtschaftsraum zuvor gek&uuml;rzt.
+      </p>
+      <p>
+        Nur in Ausnahmef&auml;llen wird die volle IP-Adresse an einen Server von
+        Google in den USA &uuml;bertragen und dort gek&uuml;rzt. Im Auftrag des
+        Betreibers dieser Website wird Google diese Informationen benutzen, um
+        Ihre Nutzung der Website auszuwerten, um Reports &uuml;ber die
+        Websiteaktivit&auml;ten zusammenzustellen und um weitere mit der
+        Websitenutzung und der Internetnutzung verbundene Dienstleistungen
+        gegen&uuml;ber dem Websitebetreiber zu erbringen. Die im Rahmen von
+        Google Analytics von Ihrem Browser &uuml;bermittelte IP-Adresse wird
+        nicht mit anderen Daten von Google zusammengef&uuml;hrt.
+      </p>
+      <p>
+        Sie k&ouml;nnen die Speicherung der Cookies durch eine entsprechende
+        Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch
+        darauf hin, dass Sie in diesem Fall gegebenenfalls nicht s&auml;mtliche
+        Funktionen dieser Website vollumf&auml;nglich werden nutzen k&ouml;nnen.
+        Sie k&ouml;nnen dar&uuml;ber hinaus die Erfassung der durch das Cookie
+        erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer
+        IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google
+        verhindern, indem sie das unter dem folgenden Link verf&uuml;gbare
+        Browser-Plugin herunterladen und installieren:
+        <a href="http://tools.google.com/dlpage/gaoptout?hl=de" target="_blank">
+          http://tools.google.com/dlpage/gaoptout?hl=de
+        </a>
+      </p>
+      <p>
+        <strong>
+          Datenschutzerkl&auml;rung f&uuml;r die Nutzung von Google Adsense
+        </strong>
+      </p>
+      <p>
+        Diese Website benutzt Google AdSense, einen Dienst zum Einbinden von
+        Werbeanzeigen der Google Inc. ("Google"). Google AdSense verwendet sog.
+        "Cookies", Textdateien, die auf Ihrem Computer gespeichert werden und
+        die eine Analyse der Benutzung der Website erm&ouml;glicht. Google
+        AdSense verwendet auch so genannte Web Beacons (unsichtbare Grafiken).
+        Durch diese Web Beacons k&ouml;nnen Informationen wie der
+        Besucherverkehr auf diesen Seiten ausgewertet werden.
+      </p>
+      <p>
+        Die durch Cookies und Web Beacons erzeugten Informationen &uuml;ber die
+        Benutzung dieser Website (einschlie&szlig;lich Ihrer IP-Adresse) und
+        Auslieferung von Werbeformaten werden an einen Server von Google in den
+        USA &uuml;bertragen und dort gespeichert. Diese Informationen
+        k&ouml;nnen von Google an Vertragspartner von Google weiter gegeben
+        werden. Google wird Ihre IP-Adresse jedoch nicht mit anderen von Ihnen
+        gespeicherten Daten zusammenf&uuml;hren.
+      </p>
+      <p>
+        Sie k&ouml;nnen die Installation der Cookies durch eine entsprechende
+        Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch
+        darauf hin, dass Sie in diesem Fall gegebenenfalls nicht s&auml;mtliche
+        Funktionen dieser Website voll umf&auml;nglich nutzen k&ouml;nnen. Durch
+        die Nutzung dieser Website erkl&auml;ren Sie sich mit der Bearbeitung
+        der &uuml;ber Sie erhobenen Daten durch Google in der zuvor
+        beschriebenen Art und Weise und zu dem zuvor benannten Zweck
+        einverstanden.
+      </p>
+      <p>
+        <strong>
+          Datenschutzerkl&auml;rung f&uuml;r die Nutzung von Google +1
+        </strong>
+      </p>
+      <p>
+        <em>Erfassung und Weitergabe von Informationen:</em><br />
+        Mithilfe der Google +1-Schaltfl&auml;che k&ouml;nnen Sie Informationen
+        weltweit ver&ouml;ffentlichen. &uuml;ber die Google +1-Schaltfl&auml;che
+        erhalten Sie und andere Nutzer personalisierte Inhalte von Google und
+        unseren Partnern. Google speichert sowohl die Information, dass Sie
+        f&uuml;r einen Inhalt +1 gegeben haben, als auch Informationen &uuml;ber
+        die Seite, die Sie beim Klicken auf +1 angesehen haben. Ihre +1
+        k&ouml;nnen als Hinweise zusammen mit Ihrem Profilnamen und Ihrem Foto
+        in Google-Diensten, wie etwa in Suchergebnissen oder in Ihrem
+        Google-Profil, oder an anderen Stellen auf Websites und Anzeigen im
+        Internet eingeblendet werden.<br />
+        Google zeichnet Informationen &uuml;ber Ihre +1-Aktivit&auml;ten auf, um
+        die Google-Dienste f&uuml;r Sie und andere zu verbessern. Um die Google
+        +1-Schaltfl&auml;che verwenden zu k&ouml;nnen, ben&ouml;tigen Sie ein
+        weltweit sichtbares, &ouml;ffentliches Google-Profil, das zumindest den
+        f&uuml;r das Profil gew&auml;hlten Namen enthalten muss. Dieser Name
+        wird in allen Google-Diensten verwendet. In manchen F&auml;llen kann
+        dieser Name auch einen anderen Namen ersetzen, den Sie beim Teilen von
+        Inhalten &uuml;ber Ihr Google-Konto verwendet haben. Die Identit&auml;t
+        Ihres Google-Profils kann Nutzern angezeigt werden, die Ihre
+        E-Mail-Adresse kennen oder &uuml;ber andere identifizierende
+        Informationen von Ihnen verf&uuml;gen.<br />
+        <br />
+        <em>Verwendung der erfassten Informationen:</em><br />
+        Neben den oben erl&auml;uterten Verwendungszwecken werden die von Ihnen
+        bereitgestellten Informationen gem&auml;&szlig; den geltenden
+        Google-Datenschutzbestimmungen genutzt. Google ver&ouml;ffentlicht
+        m&ouml;glicherweise zusammengefasste Statistiken &uuml;ber die
+        +1-Aktivit&auml;ten der Nutzer bzw. gibt diese an Nutzer und Partner
+        weiter, wie etwa Publisher, Inserenten oder verbundene Websites.
+      </p>
+      <p>
+        <em
+          >Quellenangaben:
+          <a
+            href="http://www.e-recht24.de/muster-disclaimer.htm"
+            target="_blank"
+          >
+            Disclaimer eRecht24
+          </a>
+          ,
+          <a
+            href="http://www.e-recht24.de/artikel/datenschutz/6590-facebook-like-button-datenschutz-disclaimer.html"
+            target="_blank"
+          >
+            Facebook-Disclaimer von eRecht24
+          </a>
+          ,
+          <a
+            href="http://www.google.com/intl/de_ALL/analytics/tos.html"
+            target="_blank"
+          >
+            Google Analytics Bedingungen
+          </a>
+          ,
+          <a
+            href="http://www.e-recht24.de/artikel/datenschutz/6635-datenschutz-rechtliche-risiken-bei-der-nutzung-von-google-analytics-und-googleadsense.html"
+            target="_blank"
+          >
+            Google Adsense Datenschutzerkl&auml;rung
+          </a>
+          ,
+          <a
+            href="http://www.google.com/intl/de/+/policy/+1button.html"
+            target="_blank"
+          >
+            Google +1 Datenschutzerkl&auml;rung
+          </a>
+        </em>
+      </p>
+    </b-container>
+  </div>
+</template>
