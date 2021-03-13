e02c316a37bbe762fd64c599cd74433473e1dab3
Manuel Schierenberg <mail@elanum.de>
2021-03-13 14:40:19 +0100

    build: :lipstick: add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import { BootstrapVue } from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import './app.scss';
+
+library.add(faUsers);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
