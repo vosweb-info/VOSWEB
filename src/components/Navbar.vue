808eef797376a2c20413c1b9d795583c3dfef372
Manuel Schierenberg <mail@elanum.de>
2021-03-14 16:46:20 +0100

    fix: :bug: fix data-spy offset
    relates to: #7

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,7 @@
     type="light"
     variant="light"
     class="shadow-sm"
+    v-b-scrollspy="navbarOffset * 2"
     sticky
   >
     <b-container>
@@ -15,16 +16,24 @@
         <b-navbar-toggle target="nav-collapse" />
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav v-b-scrollspy:home="70">
-            <b-nav-item href="#about"> Über uns </b-nav-item>
-            <b-nav-item href="#solutions">IT-Lösungen</b-nav-item>
-            <b-nav-item href="#maintenance">
+          <b-navbar-nav>
+            <b-nav-item href="#about" @click="scroll"> Über uns </b-nav-item>
+            <b-nav-item href="#solutions" @click="scroll"
+              >IT-Lösungen</b-nav-item
+            >
+            <b-nav-item href="#maintenance" @click="scroll">
               Administration/Wartung
             </b-nav-item>
-            <b-nav-item href="#security"> IT-Sicherheit </b-nav-item>
-            <b-nav-item href="#office365"> Office365-Integration </b-nav-item>
-            <b-nav-item href="#hosting"> Hosting/Webdesign </b-nav-item>
-            <b-nav-item href="#contact"> Kontakt </b-nav-item>
+            <b-nav-item href="#security" @click="scroll">
+              IT-Sicherheit
+            </b-nav-item>
+            <b-nav-item href="#office365" @click="scroll">
+              Office365-Integration
+            </b-nav-item>
+            <b-nav-item href="#hosting" @click="scroll">
+              Hosting/Webdesign
+            </b-nav-item>
+            <b-nav-item href="#contact" @click="scroll"> Kontakt </b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </div>
@@ -36,5 +45,20 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Navbar extends Vue {}
+export default class Navbar extends Vue {
+  navbarOffset = 70;
+
+  scroll(event: Event) {
+    event.preventDefault();
+    const target = (event.target as HTMLTextAreaElement);
+    const href = target.getAttribute('href');
+    const element = href ? document.querySelector(href) : null;
+    if (element) {
+      const offset = element.getBoundingClientRect().top
+        + window.pageYOffset
+        - this.navbarOffset;
+      window.scrollTo({ top: offset, behavior: 'smooth' });
+    }
+  }
+}
 </script>
