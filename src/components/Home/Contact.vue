30b8678340cea153f7fadfc88336b47a4e3a2b20
Manuel Schierenberg <mail@elanum.de>
2021-03-20 23:29:05 +0100

    feat: :heavy_plus_sign: add google recaptcha
    refers to: #2

--- src/components/Home/Contact.vue
+++ src/components/Home/Contact.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
-    <b-container>
+    <b-container class="text-center">
       <h2>Kontakt</h2>
-      <b-form>
+      <b-form :model="form" @submit.prevent="submit" ref="contactForm">
         <b-row>
           <b-form-group class="col">
             <b-form-input type="text" placeholder="Name" required />
@@ -21,11 +21,19 @@
         </b-row>
         <b-row>
           <b-form-group class="col">
-            <b-form-textarea placeholder="Ihre Nachricht" rows="5" />
+            <b-form-textarea placeholder="Ihre Nachricht" rows="5" required/>
           </b-form-group>
         </b-row>
         <b-row>
           <b-form-group class="col">
+            <VueRecaptcha
+              ref="recaptcha"
+              sitekey="sitekey"
+              loadRecaptchaScript
+              @verify="onVerify"
+            />
+          </b-form-group>
+          <b-form-group class="col text-right">
             <b-button type="submit" variant="secondary">Senden</b-button>
           </b-form-group>
         </b-row>
@@ -33,3 +41,30 @@
     </b-container>
   </section>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import VueRecaptcha from 'vue-recaptcha';
+
+@Component({
+  components: {
+    VueRecaptcha,
+  },
+})
+export default class Contact extends Vue {
+  sitekey = process.env.GOOGLE_CAPTCHA;
+
+  form = { robot: false };
+
+  submit() {
+    if (this.form.robot) {
+      console.log(this.form);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onVerify(response: any) {
+    if (response) this.form.robot = true;
+  }
+}
+</script>
